e1f60f47bdc72e88e21d05da851eddd9e3b27659
Rmilla <robin.milla.pro@gmail.com>
2024-04-29 17:02:02 +0200

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { AjoutClientComponent } from './ajout-client/ajout-client.component';
 import { AuthentificationComponent } from './authentification/authentification.component';
 import { StatCampingComponent } from './stat-camping/stat-camping.component';
 import { StatGeneralComponent } from './stat-general/stat-general.component';
+import { authguardGuard } from './authguard.guard';
 
 export const routes: Routes = [
-    { path: '', component: AuthentificationComponent },
-    { path: 'ajout-client', component: AjoutClientComponent },
-    { path: 'stat-camping', component: StatCampingComponent },
-    { path: 'stat-general', component: StatGeneralComponent }
+    { path: '', redirectTo:'authentification', pathMatch:'full' },
+    { path: 'authetification', component: AuthentificationComponent, canActivate: [authguardGuard],},
+    { path: 'ajout-client', component: AjoutClientComponent, canActivate: [authguardGuard], },
+    { path: 'stat-camping', component: StatCampingComponent, canActivate: [authguardGuard], },
+    { path: 'stat-general', component: StatGeneralComponent, canActivate: [authguardGuard], }
     // { path: '**', component: PageNotFoundComponent }
 ];
