3b3798df45949ff171cf0264ffe006beed56ec25
Rmilla <robin.milla.pro@gmail.com>
2024-05-07 09:33:53 +0200

    créations du composant page-not-found.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,12 @@ import { AjoutClientComponent } from './ajout-client/ajout-client.component';
 import { AuthentificationComponent } from './authentification/authentification.component';
 import { StatGeneralComponent } from './stat-general/stat-general.component';
 import { authguardGuard } from './authguard.guard';
+import { PageNotFoundComponentComponent } from './page-not-found-component/page-not-found-component.component';
 
 export const routes: Routes = [
     { path: '', redirectTo:'/authentification', pathMatch:'full' },
     { path: 'authentification', component: AuthentificationComponent, canActivate: [authguardGuard]},
     { path: 'ajout-client', component: AjoutClientComponent, canActivate: [authguardGuard] },
-    { path: 'stat-general', component: StatGeneralComponent, canActivate: [authguardGuard] }
-    // { path: '**', component: PageNotFoundComponent }
+    { path: 'stat-general', component: StatGeneralComponent, canActivate: [authguardGuard] },
+    { path: '**', component: PageNotFoundComponentComponent }
 ];
